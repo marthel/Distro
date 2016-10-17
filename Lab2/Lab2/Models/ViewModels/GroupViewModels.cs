70534adc5bd13b90059ff1b09ac3fcb3670b8364
marthel <marthel@kth.se>
2016-10-17 14:15:17 +0200

    asdasdasd

---
+++ Lab2/Lab2/Models/ViewModels/GroupViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Models.ViewModels
+{
+    public class JoinGroupViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid UserId { get; set; }
+    }
+    public class CreateGroupViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string Creater { get; set; }
+    }
+    public class EditGroupViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public Guid GroupId { get; set; }
+        [Required]
+        [Display(Name = "New Name")]
+        public string NewName { get; set; }
+    }
+}
--- Lab2/Lab2/Models/ViewModels/MessageViewModels.cs
+++ Lab2/Lab2/Models/ViewModels/MessageViewModels.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Lab2.Models.ViewModels
 {
@@ -12,22 +13,33 @@ namespace Lab2.Models.ViewModels
         [Display(Name = "Text")]
         public string Text { get; set; }
 
-        [Display(Name = "User Recivers")]
-        public List<string> Reciver { get; set; }
+        [HiddenInput(DisplayValue = false)]
+        public string SenderId { get; set; }
 
-        [Display(Name = "Reciver Or Recivers")]
-        public List<string> GroupRecivers { get; set; }
+        [Display(Name = "User receiver(s)")]
+        public List<string> UserReceivers { get; set; }
+
+        [Display(Name = "Group receiver(s)")]
+        public List<string> GroupReceivers { get; set; }
     }
 
     public class InboxMessageViewModel
     {
-
         [Display(Name = "Text")]
         public string Text { get; set; }
 
         [Display(Name = "Sender")]
         public string Sender { get; set; }
+    }
+    public class SentMessageViewModel
+    {
+        [Required]
+        [Display(Name = "Text")]
+        public string Text { get; set; }
 
+        [Display(Name = "Receiver(s)")]
+        public List<string> Receivers { get; set; }
 
     }
+
 }
